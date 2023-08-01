3c01ead05238b7dfa90e5f73061db7ffb8daf8a8
koki3141 <shimohara7192@gmail.com>
2023-08-01 13:12:38 +0900

    real perfect

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 
 git clone https://github.com/WongKinYiu/yolov7.git ./workspace/yolov7
 
-mkdir workspace/yolov7/weights && \
-    wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7_training.pt -P ./workspace/yolov7/weights
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7_training.pt -P ./workspace/yolov7/weights
 
 
-mkdir data/original
-mkdir data/processed
+mkdir -p workspace/data/original
+mkdir -p workspace/data/processed
 
 docker compose build
 
