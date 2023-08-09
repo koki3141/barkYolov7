afea34e3b0d71c36636b5a3764fa3b89fb7a2076
koki3141 <shimohara7192@gmail.com>
2023-08-09 17:06:48 +0900

    preresultsave

--- resultsave.py
+++ resultsave.py
@@ -6,67 +6,72 @@ from pathlib import Path
 class Setting:
     def __init__(self):
         self.train_project="20221011" # weight 20221011 8ahkmnuyy 22dip8barks
-        
-        self.evaluate_project="20230705"
+        self.valid_project="20230705"
         self.numbers=5
         self.epoch_min=500
         self.epoch_step=100
         self.epoch_max=500
+        class SaveFIle:
+            def __init__(self):
+                self.accuracy_file="accuracy.txt"
+                self.confusion_matrix_file="normalize_result_matrix.csv"
+        self.save_file=SaveFIle()
 
-    def add(self,number_count,epoch_count):
+    
+    def valid_saved(self,number_count,epoch_count):
         self.train_number_count="0"#str(number_count)
         self.train_epoch_count=str(epoch_count)
         self.valid_number_count=str(number_count)
+        train_path=Path('runs')/'train'/self.train_project/self.train_number_count/'weights'
+        self.weights = str(train_path / f"epoch_{setting.train_epoch_count}.pt")
+        self.train_data= str(f"data/processed/{self.train_project}/{self.train_number_count}/dataset.yaml")
+        self.data = str(f"data/processed/{self.valid_project}/{self.valid_number_count}/dataset.yaml")
+        self.task='val'#helpp='train, val, test, speed or study')
+        self.device="0"#helpp='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+        self.project='runs/valid'#helpp='save to project/name')
+        self.name = str(f"{self.valid_project}/{self.valid_number_count}/{self.train_project}/{self.train_number_count}")
+        self.save_file=self.save_file
+        
+
+class MeanValid:
+    def __init__(self,Setting):
+        self.setting=Setting
+            
+    def mean_accuracy(self):
+        mean_data=MeanValid.mean_data(self.save_file.accuracy_file)
+        
+        return mean_data
+            
+    def mean_confusion_matrix(self):
+        mean_data=MeanValid.mean_data(self.save_file.confusion_matrix_file)
+        
+        return mean_data
+    
+    def mean_data(self,file_name):
+        
+        def file_load(self,file_name):
+            load_path=f"{self.project}/{self.name}/{file_name}"
+            data=np.load(load_path)
+            return data
+        
+        data_list=[]    
+        
+        for number_count in range(setting.numbers):
+            setting.valid_saved(number_count,0)
+            data=file_load(self,file_name)
+            data_list+=[data]
+        mean_data=sum(data_list)/len(data_list)
+        
+        return mean_data
+        
+
+        
+        
+        
+        
 
 setting=Setting()
+mean_valid=MeanValid(setting)
+mean_valid.mean_accuracy()
 
-for number_count in range(setting.numbers):
-
-    # train_data=None
-    class ValidSetting:
-        def __init__(self,setting):
-            # savetype="best"
-            train_path=Path('runs')/'train'/setting.train_project/setting.train_number_count/'weights'
-            # self.weights=str(list(weight_path.glob(str(savetype)+"*"))[0])
-            self.weights = str(train_path / f"epoch_{setting.train_epoch_count}.pt")
-            
-            self.train_data= str(f"data/processed/{setting.train_project}/{setting.train_number_count}/dataset.yaml")
-            self.data = str(f"data/processed/{setting.evaluate_project}/{setting.valid_number_count}/dataset.yaml")
-            
-            self.batch_size=16#helpp='size of each image batch')
-            self.img_size=[320]#helpp='inference size (pixels)')
-            self.conf_thres=0.5#helpp='object confidence threshold')
-            self.iou_thres=0.45#helpp='IOU threshold for NMS')
-            self.conf_result=0.5
-            self.iou_result=0.5
-            save_inc_img=False
-            self.task='val'#helpp='train, val, test, speed or study')
-            self.device="0"#helpp='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-            self.single_cls=None#helpp='treat as single-class dataset')
-            self.augment=None#helpp='augmented inference')
-            self.verbose=None#helpp='report mAP by class')
-            self.save_txt=False #helpp='save results to *.txt')
-            self.save_hybrid=False #helpp='save label+prediction hybrid results to *.txt')
-            self.save_conf=None#helpp='save confidences in --save-txt labels')
-            self.save_json=None#helpp='save a cocoapi-compatible JSON results file')
-            self.project='runs/valid/'#helpp='save to project/name')
-            if setting.train_project == setting.evaluate_project:
-                self.name = str(f"{setting.train_project}/{setting.train_number_count}")
-            else:
-                self.name = str(f"{setting.train_project}/{setting.train_number_count}/{setting.evaluate_project}/{setting.valid_number_count}")
-            self.exist_ok=True#helpp='existing project/name ok, do not increment')
-            self.no_trace=None#helpp='don`t trace model')
-            self.v5_metric=None#helpp='assume maximum recall as 1.0 in AP calculation')
-            self.trace=None
-            self.plots=True
-            self.model=None
-            self.half_precision=True
-            self.wandb_logger=None
-            self.compute_loss=None
-            self.save_inc_img=False
-            self.save_result=True
-            
-    setting.add(number_count,0)
-    opt=ValidSetting(setting) 
-    print(opt.project+opt.name) 
     
